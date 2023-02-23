963e6b2e7fdeff60fe59793fbf0b25bed435dba8
Jeremy Taylor <jtaylor@jctaylor.org>
2023-02-23 11:38:54 -0600

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Jeremy Taylor on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
